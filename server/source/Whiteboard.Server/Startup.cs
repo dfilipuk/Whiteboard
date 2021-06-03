99a5c97ee8e7d54c0a60d65c547d4a798e7edd32
Dmitry Filipuk <kupilifds@gmail.com>
2021-06-03 22:01:34 +0300

    Add backend skeleton

---
+++ server/source/Whiteboard.Server/Startup.cs
@@ -0,0 +1,36 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Whiteboard.Server
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddHealthChecks();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapHealthChecks("/status");
+                endpoints.MapGet("/{**path}", context =>
+                {
+                    context.Response.Redirect("/status");
+                    return Task.CompletedTask;
+                });
+            });
+        }
+    }
+}
