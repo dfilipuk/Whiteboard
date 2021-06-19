47c2df0d132ef5ab30a31702aab63a028dfc14d2
Dmitry Filipuk <kupilifds@gmail.com>
2021-06-19 20:40:58 +0300

    popup skeleton

--- client/source/src/panels/Sidebar.tsx
+++ client/source/src/panels/Sidebar.tsx
@@ -6,6 +6,9 @@ export function Sidebar() {
       <i className="sidebar__button las la-pen" style={{gridArea: '1 / 1 / 1 / 1'}}></i>
       <i className="sidebar__button las la-pencil-ruler" style={{gridArea: '2 / 1 / 2 / 1'}}></i>
       <i className="sidebar__button las la-fill-drip" style={{gridArea: '3 / 1 / 3 / 1'}}></i>
+      <div className="popup">
+        <div className="popup__content"></div>
+      </div>
     </div>
   );
 }
