c52a04ea7560b0e9fe33271b7b4e48e688a82f29
Dzmitry Filipuk <dzmitry@filipuk.me>
2021-12-22 14:48:13 +0300

    Styled components (#25)

--- client/source/src/components/SizePicker.tsx
+++ client/source/src/components/SizePicker.tsx
@@ -1,22 +1,34 @@
 import React, { useCallback } from 'react';
 import Slider from 'rc-slider';
+import styled from 'styled-components';
 
 import 'rc-slider/assets/index.css';
 
-const SizePicker: React.FC = () => {
+const Container = styled.div`
+  padding: 0.1em 0.2em;
+  border-radius: 0.1em;
+`;
+
+type Props = {
+  className?: string;
+};
+
+const SizePicker: React.FC<Props> = ({ className }) => {
   const SliderWithTooltip = Slider.createSliderWithTooltip(Slider);
 
   const formatTip = useCallback((value: number) => `${value}px`, []);
   const updateValue = useCallback((value: number) => console.log(value), []);
 
   return (
-    <SliderWithTooltip
-      min={1}
-      max={100}
-      defaultValue={1}
-      tipFormatter={formatTip}
-      onAfterChange={updateValue}
-    />
+    <Container className={className}>
+      <SliderWithTooltip
+        min={1}
+        max={100}
+        defaultValue={1}
+        tipFormatter={formatTip}
+        onAfterChange={updateValue}
+      />
+    </Container>
   );
 };
 
